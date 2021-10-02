9fa91e31859adb02b3dc8a1a6ad3217d07e1a7d4
Luke Cotterell <Lukecotterell@gmail.com>
2021-10-02 14:08:36 +0100

    Removed file; improved writer

--- Writer.py
+++ Writer.py
@@ -4,75 +4,73 @@ from pwn import *
 import argparse
 import os 
 
+
+
+def green(text):
+    return(f"\033[92m{text}\033[0m")
+
 class Writeup():
     def __init__(self, Args, Client):
         self.Machine_ID = Args.machine
         self.Challenge_ID = Args.challenge
         self.Client = Client
-        self.Home = os.getenv("HOME")
-
-        if self.Machine_ID and self.Challenge_ID:
-            log.Error("Only one flag should be given; please retry with just one")
-            exit()
-        if self.Machine_ID: self.Machine()
-        elif self.Challenge_ID: self.Challenge()
-
-    def Machine(self):
-        if self.Machine_ID:
-            log.progress("Recived Machine ID")
-            Client = self.Client 
-            MachineObj = Client.get_machine(self.Machine_ID)
+        self.Home = os.getenv("HOME") #* for Windows use "%UserProfile%"
             
+        if self.Machine_ID: 
+            self.MachineWriteup()
+        
+        if self.Challenge_ID: 
+            self.Challenge()
 
-            Filename = f"{self.Home}/HTB/Writeups/Machines/{MachineObj.name}.md"
-            if self.Machine_ID <= 3:
-                MachineIP = "10.10.10." + str(Machine_ID+3)
-            else:
-                MachineIP = self.Machine_ID - 3
-            with open(Filename,"w+") as f:
-                f.write(f"""
-HTB - {MachineObj.name.capitalize()} [{MachineObj.difficulty}]
+        else: 
+            log.Error("ID Argument Missing")
 
-**Name** : {MachineObj.name} 
-**Difficulty** : {MachineObj.difficulty} 
-**OS** : {MachineObj.os}
-**URL** : [{"https://app.hackthebox.eu/machines/" + str(self.Machine_ID)}]({"https://app.hackthebox.eu/machines/" + str(self.Machine_ID)})
-**Points** : {MachineObj.points}            
-**Stars** : {MachineObj.stars}
-**Internal IP** : 10.10.10.{MachineIP}""")
+    def MachineWriteup(self):
+        for number in self.Machine_ID:
+            try:
+                log.progress(f"Machine ID : {number}")
+                MachineObj = self.Client.get_machine(number)
+                Filename = f"D:\\HTB\\{number}.md"
+                
+                if number <= 3:
+                    MachineIP = "10.10.10." + str(number)
+                else:
+                    MachineIP = "10.10.10." + str(number - 3) #! May Need Fixing 
+                    
+                with open(Filename,"w+") as f:
+                    f.write(f"""HTB - {MachineObj.name.capitalize()} [{MachineObj.difficulty}]\n\n**Name** : {MachineObj.name}\n**Difficulty** : {MachineObj.difficulty}\n**OS** : {MachineObj.os}
+    **URL** : [{"https://app.hackthebox.eu/machines/" + str(self.Machine_ID)}]({"https://app.hackthebox.eu/machines/" + str(self.Machine_ID)})\n**Points** : {MachineObj.points}\n**Stars** : {MachineObj.stars}\n**Internal IP** : 10.10.10.{MachineIP}""")
 
-            log.progress(f"Outputted to {Filename}")
+                log.progress(f"    Written to {Filename}\n")
+            except errors.NotFoundException: 
+                log.info("Machine with this ID does not exist.\n")
 
 
 
     def Challenge(self):
-        if self.Challenge_ID:
-            log.progress("Recieved Challenge ID")
-            Client = self.Client
-            ChallengeObj = Client.get_challenge(self.Challenge_ID)
+        for number in self.Challenge_ID:
+            try:
+                log.progress(f"Challenge ID : {number}")
+                ChallengeObj = self.Client.get_challenge(self.Challenge_ID)
 
-            Filename = f"{self.Home}/HTB/Writeups/Challenges/{ChallengeObj.name}.md"
-            with open(Filename,"a+") as f:
-                f.write(f"""
-HTB - {ChallengeObj.name.capitalize()} [{ChallengeObj.category}]
+                Filename = f"D:\\HTB\\{ChallengeObj.name}.md"
+                with open(Filename,"a+") as f:
+                    f.write(f"""HTB - {ChallengeObj.name.capitalize()} [{ChallengeObj.category}]\n\n**Name** : {ChallengeObj.name}\n**Category** : {ChallengeObj.category}\n**Difficulty** : {ChallengeObj.difficulty}
+    **Description** : `{ChallengeObj.description}`\n**URL** : [{"https://app.hackthebox.eu/challenge/" + str(self.Challenge_ID)}]({"https://app.hackthebox.eu/challenge/" + str(self.Challenge_ID)})\n**Points** : {ChallengeObj.points}""")
 
-**Name** : {ChallengeObj.name}
-**Category** : {ChallengeObj.category}
-**Difficulty** : {ChallengeObj.difficulty}
-**Description** : `{ChallengeObj.description}`
-**URL** : [{"https://app.hackthebox.eu/challenge/" + str(self.Challenge_ID)}]({"https://app.hackthebox.eu/challenge/" + str(self.Challenge_ID)})
-**Points** : {ChallengeObj.points}""")
-
-            log.success(f"Outputted to {Filename}")
+                log.success(f"    Written to {Filename}")
+            except errors.NotFoundException: 
+                log.info("Challenge with this ID does not exist.")
 
 # Argparse 
 Parser = argparse.ArgumentParser()
-Parser.add_argument("-challenge","-c",metavar="",help="Use this to run writeup creation on a certain challenge",action="store",type=int,default=None)
-Parser.add_argument("-machine","-m",metavar="",help="Use this to run writeup creation on a certain machine",action="store",type=int,default=None)
+Parser.add_argument("-challenge","-c",metavar="",help="Use this to run writeup creation on a certain challenge",action="store",nargs="*",default=None,type=int)
+Parser.add_argument("-machine","-m",metavar="",help="Use this to run writeup creation on a certain machine",action="store",nargs="*",default=None,type=int)
 Args = Parser.parse_args()
 
 if __name__ == "__main__":
-    Password = getpass(f"{Color.QuestionColor}Input your Password \n> {Color.RESET}") # GetPass Securely 
+    Password = getpass(f"\033[93mInput your Password > \033[0m") # GetPass Securely 
     Client = HTBClient(email="",password=Password)
-    print(f"Welcome {Client.user}")
+    print(f"[*] Authenticated as {green(Client.user.name)}")
+    print(f"[*] Processing {green(len(Args.challenge))} Challenges and {green(len(Args.machine))} Machines")
     Writeup(Args,Client)
